e5506373271e28de09ea79677c52392a01247237
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-03-03 10:32:13 +0530

    Adding Task area

--- js/main.js
+++ js/main.js
@@ -21,5 +21,11 @@ $(document).ready(function() {
     const $addButton = $('<button>').attr('id', 'add-task').addClass('btn btn-primary')
         .html('<i class="bi bi-plus-lg me-1"></i>ADD').appendTo($inputGroup);
 
+    const $tasksCard = $('<div>').addClass('card mb-4').appendTo($container);
+    const $tasksCardHeader = $('<div>').addClass('card-header bg-transparent')
+        .html('<h5 class="mb-0"><i class="bi bi-list-task me-2"></i>Tasks</h5>').appendTo($tasksCard);
+    const $tasksCardBody = $('<div>').addClass('card-body p-0').appendTo($tasksCard);
+    const $tasksList = $('<ul>').attr('id', 'task-list').addClass('list-group list-group-flush').appendTo($tasksCardBody);
+
 
 });
